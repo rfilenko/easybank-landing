1496695e3d34999725e15b6ff240b183ebea7879
romanfilenko <filenko@uxf.cz>
2020-05-22 14:14:06 +0200

    fix layout shift due to error page height

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="error">
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occurred</h1>
-    <nuxt-link to="/">Home page</nuxt-link>
-  </div>
+  <main>
+    <div class="error">
+      <h1 v-if="error.statusCode === 404">Page not found</h1>
+      <h1 v-else>An error occurred</h1>
+      <nuxt-link to="/">Home page</nuxt-link>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -17,7 +19,7 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  min-height: 50vh;
+  min-height: 85vh;
 }
 .error h1 {
   font-size: 2.25rem;
